d53413b836a8ac1961862b5db19b89181c51db1b
Matthias Riße <m.risse@fz-juelich.de>
2023-01-16 07:49:27 +0100

    Remove special treatment of quotes and CRs in tests

--- tests/test_getexec.py
+++ tests/test_getexec.py
@@ -1,3 +1,4 @@
+import shlex
 from collections import defaultdict
 from contextlib import contextmanager
 
@@ -30,7 +31,9 @@ def generate_dataset():
 
 
 def test_remote_is_initialized(dataset):
-    dataset.getexec(["bash", "-c", 'echo -n -e "test" > "$1"', "test"], path="test.txt")
+    dataset.getexec(
+        ["bash", "-c", 'printf "%s" "test" > "$1"', "test"], path="test.txt"
+    )
     sibling_uuids = map(lambda x: x["annex-uuid"], dataset.siblings())
     assert datalad_getexec.remote.GETEXEC_REMOTE_UUID in sibling_uuids
 
@@ -57,25 +60,27 @@ class DatasetActions(hst.RuleBasedStateMachine):
     def teardown(self):
         self.root_dataset.remove(reckless="kill")
 
-    # NOTE: null byte removed for now, behaves weird
-    # NOTE: CR removed for now, since git normalizes to LF and the content equality does
-    # not hold
-    # NOTE: single-quote has issues because of the quoting in the echo command
+    # NOTE: null byte intentionally removed, behaves weird
     @hst.rule(
         target=files,
         data=hs.data(),
         uuid=hs.uuids(version=4),
         content=hs.text(
             alphabet=hs.characters(
-                blacklist_categories=("Cs",), blacklist_characters=["\0", "\r", "'"]
+                blacklist_categories=("Cs",), blacklist_characters=["\0"]
             )
-        ),
+        ).map(lambda x: x.replace("\r\n", "\n").replace("\r", "\n")),
     )
     def add_getexec_file(self, data, uuid, content):
         dataset = data.draw(hs.sampled_from(self.datasets))
         filename = str(uuid)
         dataset.getexec(
-            ["bash", "-c", "echo -n -e '{}' > \"$1\"".format(content), "test"],
+            [
+                "bash",
+                "-c",
+                "printf '%s' {} > \"$1\"".format(shlex.quote(content)),
+                "test",
+            ],
             path=filename,
         )
         self.files.append((filename, dataset, content))
