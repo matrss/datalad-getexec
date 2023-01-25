905fc19ac2ec44df6d54efe550b44d3601b1dbc3
Matthias Riße <m.risse@fz-juelich.de>
2023-01-25 12:20:05 +0100

    Reduce the amount of hypothesis examples in CI to 500

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,5 @@ import hypothesis as h
 from datalad.conftest import setup_package  # noqa: F401
 
 h.settings.register_profile("fast", max_examples=10)
-h.settings.register_profile("ci", max_examples=1000)
+h.settings.register_profile("ci", max_examples=500)
 h.settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "fast"))
