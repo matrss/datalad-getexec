58bb43081e430aaf149ace0c3a7d607fc451fe64
Matthias Riße <m.risse@fz-juelich.de>
2023-01-27 12:20:09 +0100

    Add test for invalid spec URL

--- tests/test_spec.py
+++ tests/test_spec.py
@@ -1,4 +1,5 @@
 import hypothesis as h
+import pytest
 
 from datalad_getexec.spec import Spec
 
@@ -6,3 +7,10 @@ from datalad_getexec.spec import Spec
 @h.given(...)
 def test_spec_dict_equality(spec: Spec) -> None:
     assert Spec.from_url(spec.to_url()) == spec
+
+
+@h.given(...)
+def test_spec_invalid_url_causes_value_error(url: str) -> None:
+    h.assume(not url.startswith("getexec:v1-"))
+    with pytest.raises(ValueError):
+        Spec.from_url(url)
