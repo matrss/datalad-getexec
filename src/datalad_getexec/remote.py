f761b25381f07ea0061797f70ac24d46dd944dd7
Matthias Riße <m.risse@fz-juelich.de>
2023-01-31 16:26:29 +0100

    Refactor URL handling

--- src/datalad_getexec/remote.py
+++ src/datalad_getexec/remote.py
@@ -5,7 +5,6 @@ from typing import List
 
 from annexremote import Master, RemoteError, SpecialRemote
 
-from datalad_getexec import utils
 from datalad_getexec.spec import Spec
 
 logger = logging.getLogger("datalad.getexec.remote")
@@ -31,6 +30,29 @@ class GetExecRemote(SpecialRemote):
     def prepare(self) -> None:
         pass
 
+    def _execute_cmd(self, cmd: List[str]) -> None:
+        logger.debug("executing %s", cmd)
+        self.annex.info("executing {}".format(cmd))
+        result = subprocess.run(cmd, stdout=subprocess.PIPE)
+        logger.info(result.stdout)
+        if result.returncode != 0:
+            raise RemoteError("Failed to execute {}".format(cmd))
+
+    def _handle_url(self, url: str, filename: str) -> None:
+        import datalad.api as da
+        from datalad.utils import swallow_outputs
+
+        spec = Spec.from_url(url)
+        inputs = spec.inputs
+        if inputs:
+            with swallow_outputs() as cm:
+                logger.info("fetching inputs: %s", inputs)
+                da.get(set(inputs))
+                logger.info("datalad get output: %s", cm.out)
+        cmd = spec.cmd
+        cmd.append(filename)
+        self._execute_cmd(cmd)
+
     def transfer_retrieve(self, key: str, filename: str) -> None:
         logger.debug(
             "%s called with key %s and filename %s",
@@ -38,48 +60,11 @@ class GetExecRemote(SpecialRemote):
             key,
             filename,
         )
-
-        def _execute_cmd(cmd: List[str]) -> None:
-            logger.debug("executing %s", cmd)
-            self.annex.info("executing {}".format(cmd))
-            result = subprocess.run(cmd, stdout=subprocess.PIPE)
-            logger.info(result.stdout)
-            if result.returncode != 0:
-                raise RemoteError("Failed to execute {}".format(cmd))
-
-        def _handle_url(url: str) -> None:
-            spec = Spec.from_url(url)
-
-            inputs = spec.inputs
-            if inputs:
-                import datalad.api as da
-                from datalad.utils import swallow_outputs
-
-                for e in inputs:
-                    result = subprocess.run(
-                        ["git", "annex", "lookupkey", e], capture_output=True
-                    )
-                    input_key = utils.removesuffix(result.stdout.decode("utf-8"), "\n")
-                    if result.returncode == 0 and input_key == key:
-                        raise HandleUrlError("Circular dependency found")
-
-                with swallow_outputs() as cm:
-                    logger.info("fetching inputs: %s", inputs)
-                    # NOTE: this might be more efficient if we collect transitive
-                    # dependencies and aggregate them in one get call
-                    da.get(inputs)
-                    logger.info("datalad get output: %s", cm.out)
-
-            cmd = spec.cmd
-            cmd.append(filename)
-            _execute_cmd(cmd)
-
         urls = self.annex.geturls(key, "getexec:")
         logger.debug("urls for this key: %s", urls)
-
         for url in urls:
             try:
-                _handle_url(url)
+                self._handle_url(url, filename)
                 break
             except HandleUrlError:
                 pass
