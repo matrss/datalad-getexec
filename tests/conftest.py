bec5906d208b71543be64dc6c533137a1ca1401b
Matthias Riße <m.risse@fz-juelich.de>
2023-02-01 10:03:52 +0100

    Increase maximum number of examples in hypothesis tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,6 @@ import os
 import hypothesis as h
 from datalad.conftest import setup_package  # noqa: F401
 
-h.settings.register_profile("fast", max_examples=10)
+h.settings.register_profile("fast", max_examples=50)
 h.settings.register_profile("ci", max_examples=500)
 h.settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "fast"))
