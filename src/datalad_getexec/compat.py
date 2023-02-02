1945b0a7403ff665fa993e860dc211d6ddd75d51
Matthias Riße <m.risse@fz-juelich.de>
2023-02-02 13:12:27 +0100

    Fix flipped pragma conditionals for coverage

--- src/datalad_getexec/compat.py
+++ src/datalad_getexec/compat.py
@@ -2,18 +2,18 @@ import sys
 
 
 def removeprefix(s: str, prefix: str) -> str:
-    if sys.version_info >= (3, 9):  # pragma: py-gte-39
+    if sys.version_info >= (3, 9):  # pragma: py-lt-39
         return s.removeprefix(prefix)
-    else:  # pragma: py-lt-39
+    else:  # pragma: py-gte-39
         if s.startswith(prefix):
             return s[len(prefix) :]
         return s
 
 
 def removesuffix(s: str, suffix: str) -> str:
-    if sys.version_info >= (3, 9):  # pragma: py-gte-39
+    if sys.version_info >= (3, 9):  # pragma: py-lt-39
         return s.removesuffix(suffix)
-    else:  # pragma: py-lt-39
+    else:  # pragma: py-gte-39
         if len(suffix) > 0 and s.endswith(suffix):
             return s[: -len(suffix)]
         return s
