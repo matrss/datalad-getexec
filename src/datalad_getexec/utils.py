d4a325a90c83b8e36b38b30a66336aed697d9bee
Matthias Riße <m.risse@fz-juelich.de>
2023-02-02 10:49:31 +0100

    Use conditional coverage for compat functions

--- src/datalad_getexec/utils.py
+++ src/datalad_getexec/utils.py
@@ -2,16 +2,18 @@ import sys
 
 
 def removeprefix(s: str, prefix: str) -> str:
-    if sys.version_info >= (3, 9):
+    if sys.version_info >= (3, 9):  # pragma: py-gte-39
         return s.removeprefix(prefix)
-    if s.startswith(prefix):
-        return s[len(prefix) :]
-    return s
+    else:  # pragma: py-lt-39
+        if s.startswith(prefix):
+            return s[len(prefix) :]
+        return s
 
 
 def removesuffix(s: str, suffix: str) -> str:
-    if sys.version_info >= (3, 9):
+    if sys.version_info >= (3, 9):  # pragma: py-gte-39
         return s.removesuffix(suffix)
-    if s.endswith(suffix):
-        return s[: -len(suffix)]
-    return s
+    else:  # pragma: py-lt-39
+        if s.endswith(suffix):
+            return s[: -len(suffix)]
+        return s
