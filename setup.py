3082fdbf3db57f97a1812bde3918356d766ab1db
Matthias Riße <m.risse@fz-juelich.de>
2023-02-06 16:58:15 +0100

    Move metadata and configuration to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+import sys
 
-import versioneer
-from _datalad_buildsupport.setup import BuildManPage
+sys.path.insert(0, "")
+
+from setuptools import setup  # noqa: E402
+
+import versioneer  # noqa: E402
+from _datalad_buildsupport.setup import BuildManPage  # noqa: E402
 
 cmdclass = versioneer.get_cmdclass()
 cmdclass.update(build_manpage=BuildManPage)
